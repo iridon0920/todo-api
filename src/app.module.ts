2a3a025dd5b404b7c984ff3d42543f0549550e0e
iridon0920 <iridon0920@gmail.com>
2022-08-04 00:48:11 +0900

    add: TypeORM導入 Userテーブル定義作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './entity/user.entity'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
